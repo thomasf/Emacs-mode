64d4d7977765f2ffff44ae17c45d97d2e05c2084
Davi Costa <davicosta@dcosta-mba.local>
2012-06-27 23:15:44 -0700

    a) Take out the yellow box for edit text areas b) Console now grows from the left so one can still see and navigate to lower areas c) Fix Command + F behavior for Mac OS d) Unbind Control + R so refresh still works

--- script.js
+++ script.js
@@ -40,7 +40,6 @@ var shortcuts = {
 	'CTRL-G': {'h': 'Cancel', 'f': function(e) { log("Hmm.. How did you get here?") } },
 	'CTRL-S': {'h': 'I-search links', 'f': function(e) { searchLinks(e, 0) } },
 	'CTRL-ALT-S': {'h': 'Regexp I-search links', 'f': function(e) { searchLinks(e, 1) } },
-	'CTRL-R': {'h': 'Reverse I-search links', 'f': function(e) { searchLinks(e, 0) } },
 	'CTRL-ALT-R': {'h': 'Reverse regexp I-search links', 'f': function(e) { searchLinks(e, 1) } },
 	'CTRL-J': {'h': 'Jump to link or form control', 'f': function(e) { jumpTo(e) } },
 	// native functions
@@ -164,7 +163,8 @@ var keydownevent = function(e) {
 	var input = '';
 	if (e.shiftKey) input += 'SHIFT-';
 	if (e.ctrlKey)  input += 'CTRL-';
-	if (e.altKey)   input += 'ALT-';
+        if (e.altKey)   input += 'ALT-';
+        if (e.metaKey)   input += 'CTRL-';
 	var key;
 	switch (e.keyCode) {
 		case 226:
